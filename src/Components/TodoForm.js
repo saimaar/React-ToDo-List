4a4539de0839860ceba9d739567b0eb3c76ef51a
saimaar <rahman.saima92@gmail.com>
2021-02-03 12:58:59 -0500

    added todo form

---
+++ src/Components/TodoForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class  TodoForm extends Component {
+    state = { 
+        text: ""
+     }
+    render() { 
+        return ( 
+            <div>
+                <input/>
+            </div>
+         );
+    }
+}
+ 
+export default TodoForm;
