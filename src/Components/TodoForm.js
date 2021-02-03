ccdb9c697c4aaf408670ae4f4952bd4d76cd771e
saimaar <rahman.saima92@gmail.com>
2021-02-03 13:09:02 -0500

    onChange func to-do form

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -4,10 +4,24 @@ class  TodoForm extends Component {
     state = { 
         text: ""
      }
+
+     handleChange=(e)=>{
+         //we can also do text: e.target.value
+         // Below code makes it more dynamic
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+     }
+
     render() { 
         return ( 
             <div>
-                <input/>
+                <input
+                    name="text"
+                    placeholder="todo.."
+                    value={this.state.text}
+                    onChange={this.handleChange}
+                />
             </div>
          );
     }
